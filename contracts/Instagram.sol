3620f8c8e4ced724764f0701161cbd4bc9ba3df4
DevAnas17 <anasdeveloper17@gmail.com>
2022-12-30 22:56:17 +0530

    add modification in contract

--- contracts/Instagram.sol
+++ contracts/Instagram.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.16;
 
 contract Instagram {
@@ -15,45 +14,38 @@ contract Instagram {
     uint256 public imageCount;
     mapping(uint256 => Image) public images;
 
-    event ImageCreated(
-        uint256 id,
-        string url,
-        string caption,
-        address payable author
-    );
-
+    event ImageCreated(uint256 id, string url, string caption, address author);
     event ImageTipped(
         uint256 id,
         string url,
         string caption,
         uint256 currentTip,
         uint256 totalTipped,
-        address payable author
+        address author
     );
 
-    function uploadImage(string memory _imgUrl, string memory _caption) public {
+    function uploadImage(string memory _url, string memory _caption) public {
         imageCount++;
 
         images[imageCount] = Image(
             imageCount,
-            _imgUrl,
+            _url,
             _caption,
             0,
             payable(msg.sender),
             new address[](0)
         );
 
-        emit ImageCreated(imageCount, _imgUrl, _caption, payable(msg.sender));
+        emit ImageCreated(imageCount, _url, _caption, payable(msg.sender));
     }
 
-    function tipImageOwner(uint256 _id) public payable {
+    function tipImage(uint256 _id) public payable {
         Image memory _image = images[_id];
-        require(_id >= 0 && _id >= imageCount, "Invalidimage ID");
-        require(msg.value > 0, "Tip must be greater than 0");
-        require(
-            msg.sender != _image.author,
-            "Owner cannot tip their own image"
-        );
+
+        require(_id > 0 && _id <= imageCount, "Invalid Image ID");
+        require(msg.value > 0, "Tip Amount must be greater than 0");
+        require(msg.sender != _image.author, "Owner cannot tip their own images");
+
         payable(_image.author).transfer(msg.value);
         _image.totalTipAmount += msg.value;
         images[_id] = _image;
@@ -66,5 +58,13 @@ contract Instagram {
             _image.totalTipAmount,
             _image.author
         );
+    }
+
+    function getImages() public view returns(Image[] memory){
+        Image[] memory _image = new Image[](imageCount);
+        for(uint256 i = 0; i < imageCount; i++){
+            _image[i] = images[i+1];
+        }
+        return _image;
     }
 }
